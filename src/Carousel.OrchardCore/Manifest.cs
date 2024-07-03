6bfad77c20a55ad904167d65844a9b251cb5c59f
Robin Weichselbraun <robin.weichselbraun@mes.wien>
2024-07-03 08:02:14 +0200

    updated manifest

--- src/Carousel.OrchardCore/Manifest.cs
+++ src/Carousel.OrchardCore/Manifest.cs
@@ -4,8 +4,8 @@ using OrchardCore.Modules.Manifest;
     Name = "Carousel Widget",
     Author = "David Hayden",
     Website = "https://www.davidhayden.me",
-    Version = "1.0.0-rc2",
-    Description = "Adds a Bootstrap 4 Carousel Widget.",
-    Dependencies = new[] {"OrchardCore.ContentFields", "OrchardCore.Flows", "OrchardCore.Media", "OrchardCore.Widgets"},
+    Version = "1.8.3",
+    Description = "Adds a Bootstrap 5 Carousel Widget.",
+    Dependencies = new[] { "OrchardCore.ContentFields", "OrchardCore.Flows", "OrchardCore.Media", "OrchardCore.Widgets" },
     Category = "Content"
 )]
